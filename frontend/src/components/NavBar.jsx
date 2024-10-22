fd9182d6d546599148ea960d0ff019372b439b46
Bonface Osuka <bonyosuks12731@gmail.com>
2024-10-22 15:02:24 +0300

    picturemart

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+  
+const NavBar = () => {
+  return (
+  <div className='flex items-center justify-between py-5 font-medium'>
+    <img src="https://firebasestorage.googleapis.com/v0/b/afia-equity.appspot.com/o/imagePost%2F44168?alt=media&token=1c46b34a-e7c4-4202-b511-573440e73e2f" 
+         alt="Description of image" 
+         className="w-36"/>
+</div>
+  )
+}
+export default NavBar
